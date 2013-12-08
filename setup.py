a9b438a01d79a7d4887b21f208f03486070d58ee
Niall Kelly <duke.sam.vimes@gmail.com>
2013-12-08 14:00:31 -0800

    Creating skeleton setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+requirements = ['requests']
+
+setup(
+    name='yapel',
+    author='Niall Kelly',
+    author_email='duke.sam.vimes@gmail.com',
+    description='A library for interacting with eve central and CCP APIs',
+    install_requires=requirements,
+    license='APLv2',
+)
